76e2d681f7b36677058ba6691b5e009b12f30bbe
EC2 Default User <ec2-user@ip-172-31-19-111.eu-west-1.compute.internal>
2018-04-27 22:31:40 +0000

    aws

--- run.sh
+++ run.sh
@@ -1,49 +1,25 @@
 #!/bin/bash
-python3 BIGNMF.py lsnmf 1 &
-python3 BIGNMF.py lsnmf 1 nndsvd &
-python3 BIGNMF.py lsnmf 1 pca &
-python3 BIGNMF.py lsnmf 1 ica &
-python3 BIGNMF.py lsnmf 1 kmeans &
-python3 BIGNMF.py lsnmf 1 skmeans &
-python3 BIGNMF.py lsnmf 2 &
-python3 BIGNMF.py lsnmf 2 nndsvd &
-python3 BIGNMF.py lsnmf 2 pca &
-python3 BIGNMF.py lsnmf 2 ica &
-python3 BIGNMF.py lsnmf 2 kmeans &
-python3 BIGNMF.py lsnmf 2 skmeans &
-python3 BIGNMF.py nmf 1 &
-python3 BIGNMF.py nmf 1 nndsvd &
-python3 BIGNMF.py nmf 1 pca &
-python3 BIGNMF.py nmf 1 ica &
-python3 BIGNMF.py nmf 1 kmeans &
-python3 BIGNMF.py nmf 1 skmeans &
-python3 BIGNMF.py nmf 2 &
-python3 BIGNMF.py nmf 2 nndsvd &
-python3 BIGNMF.py nmf 2 pca &
-python3 BIGNMF.py nmf 2 ica &
-python3 BIGNMF.py nmf 2 kmeans &
-python3 BIGNMF.py nmf 2 skmeans &
-python3 BIGNMF.py sepnmf 1 &
-python3 BIGNMF.py sepnmf 1 nndsvd &
-python3 BIGNMF.py sepnmf 1 pca &
-python3 BIGNMF.py sepnmf 1 ica &
-python3 BIGNMF.py sepnmf 1 kmeans &
-python3 BIGNMF.py sepnmf 1 skmeans &
-python3 BIGNMF.py sepnmf 2 &
-python3 BIGNMF.py sepnmf 2 nndsvd &
-python3 BIGNMF.py sepnmf 2 pca &
-python3 BIGNMF.py sepnmf 2 ica &
-python3 BIGNMF.py sepnmf 2 kmeans &
-python3 BIGNMF.py sepnmf 2 skmeans &
-python3 BIGNMF.py nsnmf 1 &
-python3 BIGNMF.py nsnmf 1 nndsvd &
-python3 BIGNMF.py nsnmf 1 pca &
-python3 BIGNMF.py nsnmf 1 ica &
-python3 BIGNMF.py nsnmf 1 kmeans &
-python3 BIGNMF.py nsnmf 1 skmeans &
-python3 BIGNMF.py nsnmf 2 &
-python3 BIGNMF.py nsnmf 2 nndsvd &
-python3 BIGNMF.py nsnmf 2 pca &
-python3 BIGNMF.py nsnmf 2 ica &
-python3 BIGNMF.py nsnmf 2 kmeans &
-python3 BIGNMF.py nsnmf 2 skmeans &
+python3.6 BIGNMF.py lsnmf 1 &
+python3.6 BIGNMF.py lsnmf 1 nndsvd &
+python3.6 BIGNMF.py lsnmf 1 pca &
+python3.6 BIGNMF.py lsnmf 1 ica &
+python3.6 BIGNMF.py lsnmf 1 kmeans &
+python3.6 BIGNMF.py lsnmf 1 skmeans &
+python3.6 BIGNMF.py lsnmf 2 &
+python3.6 BIGNMF.py lsnmf 2 nndsvd &
+python3.6 BIGNMF.py lsnmf 2 pca &
+python3.6 BIGNMF.py lsnmf 2 ica &
+python3.6 BIGNMF.py lsnmf 2 kmeans &
+python3.6 BIGNMF.py lsnmf 2 skmeans &
+python3.6 BIGNMF.py sepnmf 1 &
+python3.6 BIGNMF.py sepnmf 1 nndsvd &
+python3.6 BIGNMF.py sepnmf 1 pca &
+python3.6 BIGNMF.py sepnmf 1 ica &
+python3.6 BIGNMF.py sepnmf 1 kmeans &
+python3.6 BIGNMF.py sepnmf 1 skmeans &
+python3.6 BIGNMF.py sepnmf 2 &
+python3.6 BIGNMF.py sepnmf 2 nndsvd &
+python3.6 BIGNMF.py sepnmf 2 pca &
+python3.6 BIGNMF.py sepnmf 2 ica &
+python3.6 BIGNMF.py sepnmf 2 kmeans &
+python3.6 BIGNMF.py sepnmf 2 skmeans &
